7fe1719efd64a63ac0e5053d01969fca04f4f61b
AnthonySzymczak <doyalajs1@live.com>
2021-10-28 16:17:04 -0400

    added css background for application, imfcodepen

--- learning-javascript/src/components/Welcome/index.js
+++ learning-javascript/src/components/Welcome/index.js
@@ -4,7 +4,9 @@ import "./index.css";
 function Welcome() {
     return (
         <div>
-            <h1>Let's have a fun learning adenture!</h1>
+            <h1>Let's have fun learning!</h1>
+            
+                    {/* <h2>Join us</h2> */}
         </div>
     );
 };
