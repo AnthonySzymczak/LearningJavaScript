caabec1466b52038ecd2f39bb5f602c041f6c769
AnthonySzymczak <doyalajs1@live.com>
2021-11-02 12:15:43 -0400

    styling and content for intro algorithns

--- learning-javascript/src/components/IntroAlgorithms/index.js
+++ learning-javascript/src/components/IntroAlgorithms/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
-// import "./intro.css"
+import "./algorithms.css"
 
 function IntroAlgorithms (){
     return(
 <div>
             <h1>Intro to Algorithms</h1>
             
-            <div>
-                <h2>Arithmetic functions</h2>
+            <div className = "arithmetic">
+                <h3>Arithmetic functions</h3>
                 
                 <p>Adding two numbers together, using variables num1 and num2</p>
                 <p>var add = function(num1, num2) [ opening curly bracket] return num1 + num2; [closing curly bracket];</p>
@@ -23,10 +23,15 @@ function IntroAlgorithms (){
                 <p>var divide = function(num1, num2) [opening curly bracket] return num1 / num2;[closing curly bracket];</p>
             </div>
 
-            <div>
-                <h2>Odd or even algorithm</h2>
+            <div className = "odd">
+                <h3>Odd or even algorithm</h3>
                 <p>Return the string "odd" if `num` is an odd number, Return "even" if `num` is even</p>
-                <p></p>
+                <p>var oddOrEven = function(num) [opening curly bracket]</p>
+                <p>if (num % 2 === 0) [opening curly bracket]</p>
+                <p>return "even";</p>
+                <p>[closing curly bracket] else [opening curly bracket]</p>
+                <p>return "odd";</p>
+                <p>[closing curly bracket] ; [closing curly bracket];</p>
             </div>
                     
         </div>
