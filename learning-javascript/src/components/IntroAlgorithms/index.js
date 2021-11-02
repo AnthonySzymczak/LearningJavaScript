a1522adf0d291844d32678340ac7b250c81208f3
AnthonySzymczak <doyalajs1@live.com>
2021-11-02 11:02:09 -0400

    beginning to add intro to algorithms

--- learning-javascript/src/components/IntroAlgorithms/index.js
+++ learning-javascript/src/components/IntroAlgorithms/index.js
@@ -6,6 +6,27 @@ function IntroAlgorithms (){
 <div>
             <h1>Intro to Algorithms</h1>
             
+            <div>
+                <h2>Arithmetic functions</h2>
+                
+                <p>Adding two numbers together, using variables num1 and num2</p>
+                <p>var add = function(num1, num2) [ opening curly bracket] return num1 + num2; [closing curly bracket];</p>
+
+                <p>Subtracting two numbers together, using variabes num1 and num2</p>
+                <p>var add = function(num1, num2) [opening curly bracket] return num1 - num2; [closing curly bracket];</p>
+                
+                
+                <p>Multiplying two numbers together, using variables num1 and num2</p>
+                <p>var add = function(num1, num2) [opening curly bracket] return num1 * num2; [closing curly bracket];</p>
+
+                <p>Dividing two numbers together, using variables num1 and num 2</p>
+                <p>var divide = function(num1, num2) [opening curly bracket] return num1 / num2;[closing curly bracket];</p>
+            </div>
+
+            <div>
+                <h2>Odd or even algorithm</h2>
+                <p>Return the string "odd" if `num` is an odd number, Return "even" if `num` is even</p>
+            </div>
                     
         </div>
     )
