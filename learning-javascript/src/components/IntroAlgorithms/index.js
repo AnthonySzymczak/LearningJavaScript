6eb8e5d98f43c282c83694830050978c4893b5de
AnthonySzymczak <doyalajs1@live.com>
2021-11-02 14:17:52 -0400

    changing info

--- learning-javascript/src/components/IntroAlgorithms/index.js
+++ learning-javascript/src/components/IntroAlgorithms/index.js
@@ -8,31 +8,32 @@ function IntroAlgorithms (){
             
             <div className = "arithmetic">
                 <h3>Arithmetic functions</h3>
-                
+                <br/>
                 <p>Adding two numbers together, using variables num1 and num2</p>
-                <p>var add = function(num1, num2) [ opening curly bracket] return num1 + num2; [closing curly bracket];</p>
-
+                <p>var add = function(num1, num2) {"{"} return num1 + num2; {"}"};</p>
+                <br/>
                 <p>Subtracting two numbers together, using variabes num1 and num2</p>
-                <p>var subtract = function(num1, num2) [opening curly bracket] return num1 - num2; [closing curly bracket];</p>
-                
+                <p>var subtract = function(num1, num2) {"{"} return num1 - num2; {"}"};</p>
+                <br/>
                 
                 <p>Multiplying two numbers together, using variables num1 and num2</p>
-                <p>var multiply = function(num1, num2) [opening curly bracket] return num1 * num2; [closing curly bracket];</p>
-
+                <p>var multiply = function(num1, num2) {"{"} return num1 * num2; {"}"};</p>
+                <br/>
                 <p>Dividing two numbers together, using variables num1 and num 2</p>
-                <p>var divide = function(num1, num2) [opening curly bracket] return num1 / num2;[closing curly bracket];</p>
+                <p>var divide = function(num1, num2) {"{"} return num1 / num2;{"}"};</p>
+            <br/>
+                <h3>Odd or even algorithm</h3>
+                <br/>
+                <p>Return the string "odd" if `num` is an odd number, Return "even" if `num` is even</p>
+                <p>var oddOrEven = function(num) {"{"}</p>
+                <p>if (num % 2 === 0) {"{"}</p>
+                <p>return "even";</p>
+                <p>{"}"} else {"{"}</p>
+                <p>return "odd";</p>
+                <p>{"}"} ; {"}"};</p>
+            
             </div>
 
-            <div className = "odd">
-                <h3>Odd or even algorithm</h3>
-                <p>Return the string "odd" if `num` is an odd number, Return "even" if `num` is even</p>
-                <p>var oddOrEven = function(num) [opening curly bracket]</p>
-                <p>if (num % 2 === 0) [opening curly bracket]</p>
-                <p>return "even";</p>
-                <p>[closing curly bracket] else [opening curly bracket]</p>
-                <p>return "odd";</p>
-                <p>[closing curly bracket] ; [closing curly bracket];</p>
-            </div>
                     
         </div>
     )
