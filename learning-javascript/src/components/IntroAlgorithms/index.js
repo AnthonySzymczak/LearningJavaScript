5e8042f291264f506082f6cf23054555c6aaf163
AnthonySzymczak <doyalajs1@live.com>
2021-11-02 11:24:45 -0400

    fixing intro errors

--- learning-javascript/src/components/IntroAlgorithms/index.js
+++ learning-javascript/src/components/IntroAlgorithms/index.js
@@ -13,11 +13,11 @@ function IntroAlgorithms (){
                 <p>var add = function(num1, num2) [ opening curly bracket] return num1 + num2; [closing curly bracket];</p>
 
                 <p>Subtracting two numbers together, using variabes num1 and num2</p>
-                <p>var add = function(num1, num2) [opening curly bracket] return num1 - num2; [closing curly bracket];</p>
+                <p>var subtract = function(num1, num2) [opening curly bracket] return num1 - num2; [closing curly bracket];</p>
                 
                 
                 <p>Multiplying two numbers together, using variables num1 and num2</p>
-                <p>var add = function(num1, num2) [opening curly bracket] return num1 * num2; [closing curly bracket];</p>
+                <p>var multiply = function(num1, num2) [opening curly bracket] return num1 * num2; [closing curly bracket];</p>
 
                 <p>Dividing two numbers together, using variables num1 and num 2</p>
                 <p>var divide = function(num1, num2) [opening curly bracket] return num1 / num2;[closing curly bracket];</p>
@@ -26,6 +26,7 @@ function IntroAlgorithms (){
             <div>
                 <h2>Odd or even algorithm</h2>
                 <p>Return the string "odd" if `num` is an odd number, Return "even" if `num` is even</p>
+                <p></p>
             </div>
                     
         </div>
